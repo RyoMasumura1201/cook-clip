1a160c0c6dfa1f16c21824546587ede8b2ebb472
ryo_of_the_ryo <ryomasumura1201@gmail.com>
2022-01-29 15:15:54 +0900

    youtube apiの呼び出しテスト

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,26 @@
 import type { NextPage } from 'next';
+import { GetStaticProps } from 'next';
 import Layout from '../components/Layout';
 
-const Home: NextPage = () => {
+type Props = {
+  data: any;
+};
+
+export const getStaticProps: GetStaticProps = async () => {
+  const res = await fetch(
+    'https://www.googleapis.com/youtube/v3/search?part=snippet&channelId=' +
+      'UCW01sMEVYQdhcvkrhbxdBpw' + //リュウジさんのchannelId
+      '&key=' +
+      process.env.YOUTUBE_API_KEY,
+  );
+  const data = await res.json();
+  return {
+    props: { data },
+  };
+};
+
+const Home: NextPage<Props> = ({ data }) => {
+  console.log(data);
   return (
     <Layout>
       <p>test</p>
