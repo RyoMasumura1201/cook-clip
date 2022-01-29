895d3d3747758de3e69767a99590b0f8d89ba9ef
ryo_of_the_ryo <ryomasumura1201@gmail.com>
2022-01-29 18:43:21 +0900

    idを変更

--- src/components/Movie.tsx
+++ src/components/Movie.tsx
@@ -7,7 +7,7 @@ type Props = {
 const Movie: React.VFC<Props> = (props) => {
   const { video } = props;
   const url = 'https://www.youtube.com/embed/' + video.id.videoId;
-  return <iframe id='ytplayer' src={url} width='480' height='270' />;
+  return <iframe id={video.id.videoId} src={url} width='480' height='270' />;
 };
 
 export default memo(Movie);
