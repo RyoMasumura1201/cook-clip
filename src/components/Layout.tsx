2cbcd6502e863e9515a9fbcd8cbc4b9d9c3183b0
ryo_of_the_ryo <ryomasumura1201@gmail.com>
2022-01-29 14:04:47 +0900

    make SEO component

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -2,6 +2,7 @@ import { memo } from 'react';
 
 import Header from './Header';
 import Footer from './Footer';
+import SEO from './SEO';
 
 type Props = {
   children: React.ReactNode;
@@ -9,6 +10,7 @@ type Props = {
 const Layout: React.VFC<Props> = ({ children }) => {
   return (
     <div className='site-wrapper'>
+      <SEO />
       <Header />
       <main>{children}</main>
       <Footer />
