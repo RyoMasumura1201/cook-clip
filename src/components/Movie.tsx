71dd1c3d32ad5e012580f0f74a01e7bc317c8d69
ryo_of_the_ryo <ryomasumura1201@gmail.com>
2022-01-30 21:53:27 +0900

    動画にタイトル追加

--- src/components/Movie.tsx
+++ src/components/Movie.tsx
@@ -1,6 +1,7 @@
 import { memo } from 'react';
 import { YoutubeMovie } from '../../type';
 import YouTube from 'react-youtube';
+import { Box, Stack, Text, VStack } from '@chakra-ui/react';
 
 type Props = {
   video: YoutubeMovie;
@@ -13,7 +14,14 @@ const opts = {
 
 const Movie: React.VFC<Props> = (props) => {
   const { video } = props;
-  return <YouTube videoId={video.id.videoId} opts={opts} />;
+  return (
+    <Box width='400px' height='300px'>
+      <Stack textAlign='center'>
+        <YouTube videoId={video.id.videoId} opts={opts} />
+        <Text>{video.snippet.title}</Text>
+      </Stack>
+    </Box>
+  );
 };
 
 export default memo(Movie);
