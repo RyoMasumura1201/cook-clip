7ae37250968eaa38a6b0bcc34a93d815848a1c28
ryo_of_the_ryo <ryomasumura1201@gmail.com>
2022-01-30 22:17:34 +0900

    titleをリンクに

--- src/components/Movie.tsx
+++ src/components/Movie.tsx
@@ -1,4 +1,5 @@
 import { memo } from 'react';
+import Link from 'next/link';
 import { YoutubeMovie } from '../../type';
 import YouTube from 'react-youtube';
 import { Box, Stack, Text, VStack } from '@chakra-ui/react';
@@ -14,11 +15,14 @@ const opts = {
 
 const Movie: React.VFC<Props> = (props) => {
   const { video } = props;
+  const url = 'movie/' + video.id.videoId;
   return (
     <Box width='400px' height='300px'>
       <Stack textAlign='center'>
         <YouTube videoId={video.id.videoId} opts={opts} />
-        <Text>{video.snippet.title}</Text>
+        <Link href={url}>
+          <a className='link'>{video.snippet.title}</a>
+        </Link>
       </Stack>
     </Box>
   );
