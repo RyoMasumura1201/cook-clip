c0af705ff4f7038314a70976697b03b950b3d1a7
ryo_of_the_ryo <ryomasumura1201@gmail.com>
2022-02-01 07:36:00 +0900

    getstaticpathsを作成

--- src/pages/movie/[id].tsx
+++ src/pages/movie/[id].tsx
@@ -1,5 +1,7 @@
+import { GetStaticPaths, GetStaticProps } from 'next';
 import { useRouter } from 'next/router';
 import Layout from '../../components/Layout';
+import { YoutubeMovie } from '../../../type';
 import { Text } from '@chakra-ui/react';
 
 export default function MoviePage() {
@@ -11,3 +13,20 @@ export default function MoviePage() {
     </Layout>
   );
 }
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  const res = await fetch(
+    'https://www.googleapis.com/youtube/v3/search?part=snippet&channelId=' +
+      'UCW01sMEVYQdhcvkrhbxdBpw' + //リュウジさんのchannelId
+      '&maxResults=10' +
+      '&key=' +
+      process.env.YOUTUBE_API_KEY,
+  );
+  const items = await res.json();
+
+  const paths = items.map((item: YoutubeMovie) => ({
+    params: { id: item.id.videoId },
+  }));
+
+  return { paths, fallback: true };
+};
