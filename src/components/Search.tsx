6cba3cdc6d111c39daee91d1ea732fa753cc295e
ryo_of_the_ryo <ryomasumura1201@gmail.com>
2022-02-03 08:28:56 +0900

    inputのonchangeイベントを追加

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -1,9 +1,13 @@
-import { memo } from 'react';
+import { memo, useState } from 'react';
 import { Button, HStack, Input } from '@chakra-ui/react';
 const Search: React.VFC = () => {
+  const [searchText, setSearchText] = useState('');
+  const onChangeSearchText = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchText(e.currentTarget.value);
+  };
   return (
     <HStack w={{ base: '90%', md: '70%' }} m='0 auto'>
-      <Input />
+      <Input value={searchText} onChange={onChangeSearchText} />
       <Button colorScheme='orange'>検索</Button>
     </HStack>
   );
