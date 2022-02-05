58cf55afdbcc1a0069bef44981710b6e17f13077
ryo_of_the_ryo <ryomasumura1201@gmail.com>
2022-02-06 08:51:47 +0900

    error文を大きく

--- src/components/Error.tsx
+++ src/components/Error.tsx
@@ -2,7 +2,7 @@ import { Text } from '@chakra-ui/react';
 
 export const Error: React.VFC = () => {
   return (
-    <Text color='red' textAlign='center' p='10'>
+    <Text color='red' textAlign='center' p='10' fontSize='large'>
       エラーが発生しました
     </Text>
   );
