f071e6dc8673c542a4462148f2082553ae5c3c79
ryo_of_the_ryo <ryomasumura1201@gmail.com>
2022-02-06 08:20:11 +0900

    loading, error時の表示を追加

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,12 @@
 import type { NextPage } from 'next';
 import { GetStaticProps } from 'next';
 import React, { useState } from 'react';
-import { Wrap, WrapItem, HStack, Input, Button, Box } from '@chakra-ui/react';
+import { Wrap, WrapItem, HStack, Input, Button, Box, Spinner, Text } from '@chakra-ui/react';
 import Layout from '../components/Layout';
 import Movie from '../components/Movie';
 import { YoutubeMovie } from '../../type';
 import { CHANNEL_ID_OF_RYUJI } from '../../const';
+import { useSearchMovie } from '../hooks/useSearchMovie';
 
 type Props = {
   items: YoutubeMovie[];
@@ -29,22 +30,15 @@ export const getStaticProps: GetStaticProps = async () => {
 const Home: NextPage<Props> = ({ items }) => {
   const [searchText, setSearchText] = useState('');
   const [movieList, setMovieList] = useState<YoutubeMovie[]>(items);
+  const { isLoading, isError, searchMovie } = useSearchMovie();
   const onChangeSearchText = (e: React.ChangeEvent<HTMLInputElement>) => {
     setSearchText(e.currentTarget.value);
   };
   const handleSearchMovie = async (e: React.MouseEvent<HTMLButtonElement>) => {
-    const res = await fetch(
-      'https://www.googleapis.com/youtube/v3/search?part=snippet&channelId=' +
-        CHANNEL_ID_OF_RYUJI +
-        '&maxResults=10' +
-        '&q=' +
-        searchText +
-        '&key=' +
-        process.env.NEXT_PUBLIC_YOUTUBE_API_KEY,
-    );
-    const data = await res.json();
-    const items = data.items;
-    setMovieList(items);
+    const items = await searchMovie(searchText);
+    if (!isError) {
+      setMovieList([...items]);
+    }
   };
   return (
     <Layout isHome>
@@ -54,13 +48,21 @@ const Home: NextPage<Props> = ({ items }) => {
           検索
         </Button>
       </HStack>
-      <Wrap justify='center' mt='4' spacing='10'>
-        {movieList?.map((item: YoutubeMovie) => (
-          <WrapItem key={item.id.videoId}>
-            <Movie video={item} />
-          </WrapItem>
-        ))}
-      </Wrap>
+      {isLoading ? (
+        <Box m='0 auto'>
+          <Spinner />
+        </Box>
+      ) : isError ? (
+        <Text color='red'>エラーが発生しました</Text>
+      ) : (
+        <Wrap justify='center' mt='4' spacing='10'>
+          {movieList?.map((item: YoutubeMovie) => (
+            <WrapItem key={item.id.videoId}>
+              <Movie video={item} />
+            </WrapItem>
+          ))}
+        </Wrap>
+      )}
     </Layout>
   );
 };
