1c1f5d8c5eb6fc14b4f1659f387d1dfdd47888ce
ryo_of_the_ryo <ryomasumura1201@gmail.com>
2022-03-13 19:53:37 +0900

    不要なインポートを削除

--- src/components/Notifications.tsx
+++ src/components/Notifications.tsx
@@ -1,9 +1,7 @@
 import { useRecoilState } from 'recoil';
-import { useToast } from '@chakra-ui/react';
 import { notificationListState } from '@/stores/notifications';
 import { NotificationToast } from './NotificationToast';
 export const Notifications: React.VFC = () => {
-  const toast = useToast();
   const [notificationList, setNotificationList] = useRecoilState(notificationListState);
 
   const dismissNotification = (id: string) => {
