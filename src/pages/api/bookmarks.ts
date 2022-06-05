302b950992e26e73de012c6b8f2697943b15d3fc
ryo_of_the_ryo <ryomasumura1201@gmail.com>
2022-06-05 21:07:59 +0900

    タイムスタンプを時間でsortするように

--- src/pages/api/bookmarks.ts
+++ src/pages/api/bookmarks.ts
@@ -23,6 +23,9 @@ export default async function handle(req: NextApiRequest, res: NextApiResponse) 
 
     const bookmarks = await prisma.bookmark.findMany({
       where: { userId: userId, videoId: videoId },
+      orderBy: {
+        startAt: 'asc',
+      },
     });
     res.json(bookmarks);
   }
