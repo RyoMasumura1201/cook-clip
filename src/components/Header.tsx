51d41739d98e1251a0c0302958fe6ae2db5f5a7c
ryo_of_the_ryo <ryomasumura1201@gmail.com>
2022-06-05 21:09:22 +0900

    不要なページを削除

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,6 @@
 import { memo } from 'react';
 import Link from 'next/link';
 import { Text } from '@chakra-ui/layout';
-import { BsBookmarkHeartFill } from 'react-icons/bs';
 import { Flex, Spacer } from '@chakra-ui/react';
 import Session from '@/components/Session';
 
@@ -19,13 +18,6 @@ const Header: React.VFC<Props> = (props) => {
             Cook Clip
           </Text>
           <Spacer />
-          <Link href='/bookmark'>
-            <a>
-              <Text color='orange' mr='3' p='3' className='link'>
-                <BsBookmarkHeartFill size='35' />
-              </Text>
-            </a>
-          </Link>
           <Session />
         </Flex>
       ) : (
@@ -38,13 +30,6 @@ const Header: React.VFC<Props> = (props) => {
             </a>
           </Link>
           <Spacer />
-          <Link href='/bookmark'>
-            <a>
-              <Text color='orange' mr='3' p='3' className='link'>
-                <BsBookmarkHeartFill size='35' />
-              </Text>
-            </a>
-          </Link>
           <Session />
         </Flex>
       )}
