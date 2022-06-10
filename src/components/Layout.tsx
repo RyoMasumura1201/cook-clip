03d45a72db0e1342c71d10c71e806ff087ddb4fe
ryo_of_the_ryo <ryomasumura1201@gmail.com>
2022-06-11 08:32:34 +0900

    Notificationsを_app.tsxに移動

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -2,12 +2,12 @@ import { memo } from 'react';
 import Header from '@/components/Header';
 import Footer from '@/components/Footer';
 import SEO from '@/components/SEO';
-import Notifications from '@/components/Notifications';
 
 type Props = {
   isHome?: boolean;
   children: React.ReactNode;
 };
+
 const Layout: React.VFC<Props> = ({ children, isHome }) => {
   return (
     <div className='site-wrapper'>
@@ -15,7 +15,6 @@ const Layout: React.VFC<Props> = ({ children, isHome }) => {
       <Header isHome={isHome} />
       <main>{children}</main>
       <Footer />
-      <Notifications />
     </div>
   );
 };
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,12 @@
 import '@/styles/globals.css';
 import { AppProps } from 'next/app';
 import { AppProvider } from '@/providers/app';
+import Notifications from '@/components/Notifications';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <AppProvider session={pageProps.session}>
+      <Notifications />
       <Component {...pageProps} />
     </AppProvider>
   );
